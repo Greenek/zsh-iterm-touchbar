1b1b21ae22654ae7279273c4e93b455beb073baf
Paweł Golonko <pgolonko@gmail.com>
2018-04-17 20:45:09 +0200

    Layout improvements

--- zsh-iterm-touchbar.plugin.zsh
+++ zsh-iterm-touchbar.plugin.zsh
@@ -3,11 +3,25 @@ GIT_UNCOMMITTED="${GIT_UNCOMMITTED:-⊕}"
 GIT_UNSTAGED="${GIT_UNSTAGED:-⊙}"
 GIT_UNTRACKED="${GIT_UNTRACKED:-⊗}"
 GIT_STASHED="${GIT_STASHED:-⊘}"
-GIT_UNPULLED="${GIT_UNPULLED:-⇣}"
-GIT_UNPUSHED="${GIT_UNPUSHED:-⇡}"
+GIT_UNPULLED="${GIT_UNPULLED:-⬇︎}"
+GIT_UNPUSHED="${GIT_UNPUSHED:-⬆︎}"
 
 # YARN
 YARN_ENABLED=false
+
+current_dir() {
+  local dir
+
+  if [[ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" == "true" ]]; then
+    local git_root=${$(git rev-parse --absolute-git-dir):h}
+    dir="$(basename $git_root)"
+  else
+    dir="$(pwd | awk -F/ -v "n=$(tput cols)" -v "h=^$HOME" '{sub(h,"~");n=0.3*n;b=$1"/"$2} length($0)<=n || NF==3 {print;next;} NF>3{b=b"/../"; e=$NF; n-=length(b $NF); for (i=NF-1;i>3 && n>length(e)+1;i--) e=$i"/"e;} {print b e;}'
+)"
+  fi
+
+  echo -n "${dir}"
+}
 
 # Output name of current branch.
 git_current_branch() {
@@ -66,10 +80,13 @@ git_unpushed_unpulled() {
 
   # counters are tab-separated, split on tab and store as array
   count=(${(ps:\t:)count})
+  echo -n ""
   local arrows left=${count[1]} right=${count[2]}
 
-  (( ${right:-0} > 0 )) && arrows+="${GIT_UNPULLED}"
-  (( ${left:-0} > 0 )) && arrows+="${GIT_UNPUSHED}"
+  (( ${right:-0} > 0 )) && arrows+="\u2B07" || arrows+="\u21E9"
+  arrows+="${right} "
+  (( ${left:-0} > 0 )) && arrows+="\u2B06" || arrows+="\u21E7"
+  arrows+="${left}"
 
   [ -n $arrows ] && echo -n "${arrows}"
 }
@@ -118,33 +135,33 @@ function _displayDefault() {
 
   # CURRENT_DIR
   # -----------
-  pecho "\033]1337;SetKeyLabel=F1=⧂ $(echo $(pwd) | awk -F/ '{print $(NF-1)"/"$(NF)}')\a"
-  bindkey -s '^[OP' 'pwd \n'
-
-  # GIT
-  # ---
-  # Check if the current directory is in a Git repository.
-  command git rev-parse --is-inside-work-tree &>/dev/null || return
+  local dir="$(current_dir)"
+  current_path="${dir}"
 
   # Check if the current directory is in .git before running git checks.
   if [[ "$(git rev-parse --is-inside-git-dir 2> /dev/null)" == 'false' ]]; then
+    pecho "\033]1337;SetKeyLabel=F1=› $current_path\a"
 
     # Ensure the index is up to date.
     git update-index --really-refresh -q &>/dev/null
 
     # String of indicators
-    local indicators="$(git_diff_shortstat)"
+    local indicators="$(git_unpushed_unpulled)"
 
     [ -n "${indicators}" ] && touchbarIndicators="${indicators}" || touchbarIndicators="✓";
 
-    pecho "\033]1337;SetKeyLabel=F2=$touchbarIndicators\a"
-    pecho "\033]1337;SetKeyLabel=F3=⋲ $(git_current_branch)\a"
+    pecho "\033]1337;SetKeyLabel=F2=\u2325 $(git_current_branch)\a"
+    pecho "\033]1337;SetKeyLabel=F3=$touchbarIndicators\a"
     pecho "\033]1337;SetKeyLabel=F4=⤓ Pull\a";
 
     # bind git actions
-    bindkey -s '^[OQ' 'git status \n'
-    bindkey -s '^[OR' 'git branch --list --all | cat'
+    bindkey -s '^[OP' 'dirname $(git rev-parse --absolute-git-dir)\n'
+    bindkey "^[OQ" _displayBranches
+    bindkey -s '^[OR' 'git status \n'
     bindkey -s '^[OS' "git pull --rebase \n"
+  else
+    pecho "\033]1337;SetKeyLabel=F1=$current_path\a"
+    bindkey -s '^[OP' 'pwd \n'
   fi
 
   if [[ -f package.json ]]; then
@@ -173,7 +190,7 @@ function _displayNpmScripts() {
   fnKeysIndex=1
   for npmScript in "$npmScripts[@]"; do
     fnKeysIndex=$((fnKeysIndex + 1))
-    bindkey -s $fnKeys[$fnKeysIndex] "npm run $npmScript \n"
+    bindkey -s $fnKeys[$fnKeysIndex] "npm run $npmScript; _displayDefault \n"
     pecho "\033]1337;SetKeyLabel=F$fnKeysIndex=$npmScript\a"
   done
 
@@ -196,7 +213,7 @@ function _displayYarnScripts() {
   fnKeysIndex=1
   for yarnScript in "$yarnScripts[@]"; do
     fnKeysIndex=$((fnKeysIndex + 1))
-    bindkey -s $fnKeys[$fnKeysIndex] "yarn run $yarnScript \n"
+    bindkey -s $fnKeys[$fnKeysIndex] "yarn run $yarnScript; _displayDefault \n"
     pecho "\033]1337;SetKeyLabel=F$fnKeysIndex=$yarnScript\a"
   done
 
@@ -218,7 +235,7 @@ function _displayBranches() {
   # for each branch name, bind it to a key
   for branch in "$gitBranches[@]"; do
     fnKeysIndex=$((fnKeysIndex + 1))
-    bindkey -s $fnKeys[$fnKeysIndex] "git checkout $branch \n"
+    bindkey -s $fnKeys[$fnKeysIndex] "git checkout $branch; _displayDefault \n"
     pecho "\033]1337;SetKeyLabel=F$fnKeysIndex=$branch\a"
   done
 
